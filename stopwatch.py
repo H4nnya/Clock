e7dae14fab101caf7a68971bdf5e81a1d837e859
Say, The Creator <64602785+H4nnya@users.noreply.github.com>
2021-01-31 13:34:11 -0300

    Add files via upload

---
+++ stopwatch.py
@@ -0,0 +1,52 @@
+import pygame
+from datetime import datetime
+
+
+def image(image_name=str()):
+    return pygame.image.load(f'numbers/{image_name}')
+
+
+def sprite(sprit_name, posx, posy):
+    return screen.blit(image(sprit_name), (posx, posy))
+
+
+pygame.init()
+screen = pygame.display.set_mode((620, 620))
+open_screen = True
+pygame.display.set_caption('Clock')
+
+while open_screen:
+    now = datetime.now()
+    hour = f'{now.hour}'
+    minute = f'{now.minute}'
+    second = f'{now.second}'
+    screen.fill(0)
+    sprite('background.png', 0, 0)
+    if len(hour) == 2:
+        sprite(f'{hour[0]}.png', 60, 250)
+        sprite(f'{hour[1]}.png', 140, 250)
+    else:
+        sprite('0.png', 60, 250)
+        sprite(f'{hour[0]}.png', 140, 250)
+
+    sprite('two_point.png', 205, 260)
+
+    if len(minute) == 2:
+        sprite(f'{minute[0]}.png', 260, 250)
+        sprite(f'{minute[1]}.png', 340, 250)
+    else:
+        sprite('0.png', 260, 250)
+        sprite(f'{minute[0]}.png', 340, 250)
+
+    if len(second) == 2:
+        sprite(f's{second[0]}.png', 445, 250)
+        sprite(f's{second[1]}.png', 485, 250)
+    else:
+        sprite('s0.png', 445, 250)
+        sprite(f's{second[0]}.png', 485, 250)
+
+    pygame.display.update()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            open_screen = False
+
